2510f11075a3face15a009af3b1b522560b6c3e0
david@arsquanta.com <david@arsquanta.com>
2014-11-23 14:36:34 -0800

    moar comment editz

--- cachematrix.R
+++ cachematrix.R
@@ -23,16 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
         setinverse <- function(inverse) {i <<- inverse}		# called to access and store the inverse value
         getinverse <- function() {i}							# will return the cashed value when accessed later
         list(set = set, get = get,							# access list for when makeCacheMatrix is called
-             setinverse = setinverse,
+             setinverse = setinverse,						
              getinverse = getinverse)
 }
 
 
-
-
-
 #The cacheSolve function will (hopefully) Return a matrix that is the inverse of 'x' cache the value 
-# and return it from the cache without re-calculating it if an idetical request is made
+# and return it from the cache without re-calculating it if an idetical request is made. 
 
 cacheSolve <- function(x, ...) {	# x is object made by makeCacheMatrix    
         i <- x$getinverse()			#  accesses the object 'x' and gets the value of the inverse
@@ -43,7 +40,7 @@ cacheSolve <- function(x, ...) {	# x is object made by makeCacheMatrix
         data <- x$get()				# this is run only if x$getinverse() returned NULL
         i <- solve(data, ...)		# calcualtes the inverse (the first time)
         x$setinverse(i)				# stores inverse matrix in "i"
-        i							# returns value of i
+        i							# returns value of i, and as the last called thing should print i
 
 
 }
