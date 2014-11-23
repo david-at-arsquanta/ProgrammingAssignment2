ae6afa38c0c1f3d2fbb98019fd24fb1a65036e5c
david@arsquanta.com <david@arsquanta.com>
2014-11-23 03:53:57 -0800

    testing 123
    yep.

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,9 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+# ok then. . .
+# this is a test of the gimpy WISIWIG GItHub interface. . . 
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
